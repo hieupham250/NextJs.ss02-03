498876056dfb569f80f1c0d9c035f5f1d115caf3
hieupham250 <ptrunghieu2507@gmail.com>
2024-08-13 21:35:01 +0700

    bai tap

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import B1 from "./B1";
+import B2 from "./B2";
+import B3 from "./B3";
+import B5 from "./B5";
+import B6 from "./B6";
+import B7 from "./B7";
+import B8 from "./B8";
+
+export default function page() {
+  return (
+    <>
+      <B1></B1>
+      <br />
+      <B2></B2>
+      <br />
+      <B3></B3>
+      <br />
+      <B5></B5>
+      <br />
+      <B6></B6>
+      <br />
+      <B7></B7>
+      <br />
+      <B8></B8>
+    </>
+  );
+}
